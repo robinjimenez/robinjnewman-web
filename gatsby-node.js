3144ea7c514334b94fcbf227bbf292b95445785a
robinjimenez <robin@robinjnewman.com>
2020-03-22 12:41:45 +0100

    Create page for each Project dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,50 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`);
+
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+  // Query for nodes to use in creating pages.
+  resolve(
+    graphql(request).then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
+      
+      return result;
+    })
+  )
+});
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  
+  const getArticles = makeRequest(graphql, `
+    {
+      allStrapiProject {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+    `).then(result => {
+    // Create pages for each article.
+    result.data.allStrapiProject.edges.forEach(({ node }) => {
+      createPage({
+        path: `/${node.slug}`,
+        component: path.resolve(`src/components/templates/ProjectPage.js`),
+        context: {
+          id: node.slug,
+        },
+      })
+    })
+  });
+  
+  // Query for articles nodes to use in creating pages.
+  return getArticles;
+};
