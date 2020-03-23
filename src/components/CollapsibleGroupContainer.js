4b48369f793e6af095359162739cec002de09bc4
robinjimenez <robin@robinjnewman.com>
2020-03-23 23:03:53 +0100

    Update Collapsible component and add styles

--- src/components/CollapsibleGroupContainer.js
+++ src/components/CollapsibleGroupContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 
 class CollapsibleItem extends Component {
   state = {
-    isCollapsed: true,
+    isCollapsed: this.props.isCollapsed
   }
 
   handleClick = () => {
@@ -13,33 +13,39 @@ class CollapsibleItem extends Component {
     const content = this.props.content
 
     return (
-      <div className="collapsible-group__item">
+      <div className={`collapsible-group__item collapsible-group__item--${
+        this.state.isCollapsed ? "collapsed" : "expanded"
+      }`}>
         <div className="collapsible-item__header">
           <h4 className="collapsible-item__title">{content.props.title}</h4>
           <button
-            className="collapsible-item__toggle"
+            className={`collapsible-item__toggle collapsible-item__toggle--${
+              this.state.isCollapsed ? "expand" : "collapse"
+            }`}
             type="button"
             onClick={this.handleClick}
           >
-            +
+            {this.state.isCollapsed ? "+" : "-"}
           </button>
         </div>
-        {this.state.isCollapsed && <div className="collapsible-item__content">{content}</div>}
+        {!this.state.isCollapsed && (
+          <div className="collapsible-item__content">{content}</div>
+        )}
       </div>
     )
   }
 }
 
 const CollapsibleGroupContainer = props => {
-    const items = props.children
+  const items = props.children
 
-    return (
-      <div className="collapsible-group">
-        {items.map(item => {
-          return <CollapsibleItem key={item.key} content={item} />
-        })}
-      </div>
-    )
+  return (
+    <div className="collapsible-group">
+      {items.map((item,index) => {
+        return <CollapsibleItem key={item.key} content={item} isCollapsed={index > 0} />
+      })}
+    </div>
+  )
 }
 
-export default CollapsibleGroupContainer;
+export default CollapsibleGroupContainer
