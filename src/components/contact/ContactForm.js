99d1b77136bb288376eb973a491b1861a20bb9e8
robinjimenez <robin@robinjnewman.com>
2020-03-24 21:08:13 +0100

    Adapt Contact Form component and add styles

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 export default class ContactForm extends React.Component {
@@ -6,8 +5,13 @@ export default class ContactForm extends React.Component {
     super(props);
     this.submitForm = this.submitForm.bind(this);
     this.state = {
-      status: ""
+      status: "",
+      name: ''
     };
+  }
+
+  greetUser = (event) => {
+    this.setState({name: event.target.value})
   }
 
   render() {
@@ -17,12 +21,24 @@ export default class ContactForm extends React.Component {
         onSubmit={this.submitForm}
         action="https://formspree.io/mbjkaqon"
         method="POST"
+        className="contact__form"
       >
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <label>Message:</label>
-        <input type="text" name="message" />
-        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+        <div className="contact-block contact-block--name">
+          <label htmlFor="name" className="name-label" >Hi, I'm </label>
+          <input type="text" name="name" id="contact-name" onBlur={this.greetUser} className="name-input" required/>
+        </div>
+        <div className="contact-block contact-block--email">
+          <label htmlFor="email" className="email-label">and my email address is </label>
+          <input type="email" name="email" id="contact-email" className="email-input" required/>
+        </div>
+        <div className="contact-block contact-block--message">
+          {this.state.name !== '' && <p className="user-greeting" >Hey there, {this.state.name}!</p>}
+          <label htmlFor="message" className="message-label">What's on your mind?</label>
+          <textarea type="text" id="contact-message" className="message-input" />
+        </div>
+        
+        <button className={`contact__submit contact__submit--${status === "SUCCESS" ? 'inactive' : 'active'}`} type="submit">{status === "SUCCESS" ? <p>Sent!</p> : 'Submit'}</button>
+        
         {status === "ERROR" && <p>Ooops! There was an error.</p>}
       </form>
     );
