e4fc630d1dbe59b7ae2633636eb03041302ab06b
robinjimenez <robin@robinjnewman.com>
2020-03-27 16:48:25 +0100

    Add Font Awesome and add social icons to Contact

--- src/components/contact/ContactContainer.js
+++ src/components/contact/ContactContainer.js
@@ -1,6 +1,8 @@
 import React from "react"
 
 import ContactForm from "./ContactForm"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLinkedin, faGithub, faDev } from "@fortawesome/free-brands-svg-icons"
 
 const ContactContainer = () => {
 
@@ -10,6 +12,11 @@ const ContactContainer = () => {
       <div className="contact-email">
         <p className="contact-paragraph">Email me at</p>
         <a className="secondary-button contact-email__link" href="mailto:hello@robinjnewman.com">hello@robinjnewman.com</a>
+      </div>
+      <div className="contact-social">
+        <p className="contact-paragraph">or find me on</p>
+        <a className="social-link" href="https://www.github.com/robinjimenez" rel="nofollow"><FontAwesomeIcon icon={faGithub} /></a>
+        <a className="social-link" href="https://www.linkedin.com/in/robinjimenez" rel="nofollow"><FontAwesomeIcon icon={faLinkedin} /></a>
       </div>
     </section>
   )
