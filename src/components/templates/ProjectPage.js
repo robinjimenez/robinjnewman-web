a45bc628097a2a3129f2897155e91e73697338bd
robinjimenez <robin@robinjnewman.com>
2020-06-17 10:23:08 +0200

    Add tags, categories and year to project detail. Tweak "Label" CSS

--- src/components/templates/ProjectPage.js
+++ src/components/templates/ProjectPage.js
@@ -9,6 +9,9 @@ export default ({ pageContext }) => {
     Title,
     Subtitle,
     Cover,
+    Year,
+    tags,
+    categories,
     Description,
     ProjectLink,
     GitHubLink,
@@ -18,6 +21,19 @@ export default ({ pageContext }) => {
     <Layout page="project-page">
       <ProjectCover title={Title} subtitle={Subtitle} cover={Cover} />
       <section className="section project-information">
+        <div className="label label--trans project-detail__year" >{ Year }</div>
+        <div className="project-detail__categories" >
+          {categories.length <= 1 ? "Category" : "Categories"}
+        { categories.map((cat)=>{
+          return (<div className="label project-detail__category">{cat.Name}</div>)
+        })}
+        </div>
+        <div className="project-detail__tags" >
+          {tags.length <= 1 ? "Tag" : "Tags"}
+        { tags.map((tag)=>{
+          return (<div className="label label--trans project-detail__tag">{tag.Name}</div>)
+        })}
+        </div>
         <div className="project-detail__links">
           <a
             href={ProjectLink}
