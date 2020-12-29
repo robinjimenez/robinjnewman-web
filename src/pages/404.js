e9c89e6deb6e44881765f9bfd12861b7eddadf48
Robin Jiménez Newman <hello@robinjnewman.com>
2020-12-29 14:48:35 +0100

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404. Not Found" />
-    <h1>Lost?</h1>
-    <p>The page you're looking for doesn't exist.</p>
+const NotFoundPage = ({location}) => (
+  <Layout location={location} page="not-found-page" >
+    <SEO title="Page not found" />
+    <section className="section not-found">
+      <h1 className="not-found__title">Hmm... <span role="img" aria-label="Thinking emoji">&#129300;</span></h1>
+      <h2 className="not-found__subtitle">The page you're looking for doesn't&nbsp;exist. Yet.</h2>
+    </section>
   </Layout>
 )
 
