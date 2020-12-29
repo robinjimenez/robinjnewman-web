ea09161fd7e835e20dd3c2c1ac3fa390ca97a01a
Robin Jiménez Newman <hello@robinjnewman.com>
2020-12-29 14:44:38 +0100

    Add favicon and PWA setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,13 +14,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    /*{
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },*/
+    },
     {
       resolve: `gatsby-source-strapi`,
       options: {
@@ -46,17 +46,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `robinjnewman`,
+        short_name: `robinjnewman`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        lang: 'en',
+        background_color: `#F3EFE8`,
+        theme_color: `#EDDBC1`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
